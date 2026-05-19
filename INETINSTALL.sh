0e96122068db63f2aec0e06af58fc038d06d3f25
Ignac <ignacyzielinski2008@gmail.com>
2026-05-19 21:05:54 +0200

    Remove warning for non-recompilation on ARM
    Removed warning about ZPM not working without recompilation for ARM architecture.

--- INETINSTALL.sh
+++ INETINSTALL.sh
@@ -216,7 +216,6 @@ rec="n"
 ARCH=$(uname -m)
 if [[ "$ARCH" == arm* ]] || [[ "$ARCH" == aarch64* ]]; then
     info "ARM architecture detected ($ARCH). Recompilation is necessary."
-    info "If you don't choose to recompile, ZPM will not work."
     ask rec "Recompile ZPM for ARM?"
 else
     info "Architecture: $ARCH (non-ARM). Skipping recompilation."
