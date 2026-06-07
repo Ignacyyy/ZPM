f26b4d14969cb4ac888fc5e475d259f6928f8260
Ignac <ignacyzielinski2008@gmail.com>
2026-06-07 17:08:58 +0200

    Fix help message formatting in zsearch.cpp

--- src/zsearch.cpp
+++ src/zsearch.cpp
@@ -20,7 +20,7 @@ string highlight(const string& text, const string& query) {
 // ─── komunikaty ───────────────────────────────────────────────────────────────
 void helpmessage(const char* progName) {
     cout << RED << "Usage: " << RESET << progName
-    << " <query> [options] or zpm search <query> [options]\n\n";
+    << " <query> [options] or zpm search <query> [options]\n";
     cout << RED << "Options:\n" << RESET;
     cout << "  -h, --help     Show help\n";
     cout << "  -v, --version  Show version\n";
