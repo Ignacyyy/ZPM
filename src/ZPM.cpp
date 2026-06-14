68ad10180c80f212b45892a0a9900bfd6081e7a8
Ignac <ignacyzielinski2008@gmail.com>
2026-06-14 14:08:52 +0200

    Refactor command structures and improve help commands

--- src/ZPM.cpp
+++ src/ZPM.cpp
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 #include <cerrno>
@@ -6,248 +7,249 @@
 
 namespace {
 
-struct CommandAlias {
-    std::string_view name;
-    std::string_view target;
-};
+    struct CommandAlias {
+        std::string_view name;
+        std::string_view target;
+    };
 
-struct CommandHelp {
-    std::string_view names;
-    std::string_view description;
-};
+    struct CommandHelp {
+        std::string_view names;
+        std::string_view description;
+    };
 
-struct ParsedArgs {
-    int commandIndex = -1;
-    int separatorIndex = -1;
-    std::string error;
-};
+    struct ParsedArgs {
+        int commandIndex = -1;
+        int separatorIndex = -1;
+        std::string error;
+    };
 
-constexpr std::array<CommandAlias, 16> kCommandAliases {{
-    {"install", "zinst"},
-    {"inst", "zinst"},
-    {"remove", "zrm"},
-    {"rm", "zrm"},
-    {"update", "zupd"},
-    {"upd", "zupd"},
-    {"upgrade", "zupgr"},
-    {"upgr", "zupgr"},
-    {"list", "zlist"},
-    {"ls", "zlist"},
-    {"search", "zsearch"},
-    {"clean", "zclean"},
-    {"info", "zinfo"},
-    {"uninstall", "zuninstall"},
-    {"home", "zhome"},
-    {"run", "zrun"},
-    {"tui", "ztui"},
-}};
+    constexpr std::array<CommandAlias, 18> kCommandAliases {{
+        {"install", "zinst"},
+        {"inst", "zinst"},
+        {"remove", "zrm"},
+        {"rm", "zrm"},
+        {"update", "zupd"},
+        {"upd", "zupd"},
+        {"upgrade", "zupgr"},
+        {"upgr", "zupgr"},
+        {"list", "zlist"},
+        {"ls", "zlist"},
+        {"search", "zsearch"},
+        {"clean", "zclean"},
+        {"info", "zinfo"},
+        {"uninstall", "zuninstall"},
+        {"home", "zhome"},
+        {"run", "zrun"},
+        {"tui", "ztui"},
+        {"ztui", "ztui"},
+    }};
 
-constexpr std::array<CommandHelp, 11> kCommandHelp {{
-    {"home", "Home page of ZPM"},
-    {"update, upd", "Perform a system upgrade"},
-    {"upgrade, upgr", "Upgrade ZPM itself"},
-    {"install, inst", "Install package"},
-    {"remove, rm", "Remove package"},
-    {"list, ls", "List installed packages"},
-    {"search", "Search for package"},
-    {"clean", "Clean system cache"},
-    {"info", "Package information"},
-    {"uninstall", "Uninstall ZPM"},
-    {"run", "Run programs using ZPM"},
-    {"tui", "Run ZPM terminal UI"},
-}};
+    constexpr std::array<CommandHelp, 12> kCommandHelp {{
+        {"home", "Home page of ZPM"},
+        {"tui, ztui", "Run ZPM terminal UI"},
+        {"update, upd", "Perform a system upgrade"},
+        {"upgrade, upgr", "Upgrade ZPM itself"},
+        {"install, inst", "Install package"},
+        {"remove, rm", "Remove package"},
+        {"list, ls", "List installed packages"},
+        {"search", "Search for package"},
+        {"clean", "Clean system cache"},
+        {"info", "Package information"},
+        {"uninstall", "Uninstall ZPM"},
+        {"run", "Run programs using ZPM"},
+    }};
 
-void showVersion() {
-    std::cout << RED << "zpm component version: v" << zpm_version::version()
-              << "\n" << RESET
-              << "https://github.com/Zielina-Konrad-productions/ZPM\n"
-              << "Copyright (c) 2026 Ignacyyy & Ry3ball\n"
-              << "License: MIT\n";
-}
-
-void showHelp() {
-    std::cout << RED << "Usage: " << RESET << "zpm <command> [options]\n";
-    std::cout << RED << "Commands:" << RESET << "\n";
-
-    for (const CommandHelp& command : kCommandHelp) {
-        std::cout << "  " << std::left << std::setw(15) << command.names
-                  << " " << command.description << "\n";
+    void showVersion() {
+        std::cout << RED << "zpm component version: v" << zpm_version::version()
+        << "\n" << RESET
+        << "https://github.com/Zielina-Konrad-productions/ZPM\n"
+        << "Copyright (c) 2026 Ignacyyy & Ry3ball\n"
+        << "License: MIT\n";
     }
 
-    std::cout << "\n" << RED << "Options:" << RESET << "\n"
-              << "  --help, -h      Show this help message\n"
-              << "  --version, -v   Show version information\n";
-}
+    void showHelp() {
+        std::cout << RED << "Usage: " << RESET << "zpm <command> [options]\n";
+        std::cout << RED << "Commands:" << RESET << "\n";
 
-bool isGlobalOption(std::string_view arg) {
-    return arg == "--help" || arg == "-h" || arg == "--version" || arg == "-v";
-}
-
-bool isOptionLike(std::string_view arg) {
-    return arg.size() > 1 && arg.front() == '-';
-}
-
-std::string commandTarget(std::string_view command) {
-    for (const CommandAlias& alias : kCommandAliases) {
-        if (alias.name == command) {
-            return std::string(alias.target);
+        for (const CommandHelp& command : kCommandHelp) {
+            std::cout << "  " << std::left << std::setw(15) << command.names
+            << " " << command.description << "\n";
         }
+
+        std::cout << "\n" << RED << "Options:" << RESET << "\n"
+        << "  --help, -h      Show this help message\n"
+        << "  --version, -v   Show version information\n";
     }
 
-    return {};
-}
+    bool isGlobalOption(std::string_view arg) {
+        return arg == "--help" || arg == "-h" || arg == "--version" || arg == "-v";
+    }
 
-ParsedArgs parseArgs(int argc, char* argv[]) {
-    ParsedArgs parsed;
-    bool optionsEnded = false;
+    bool isOptionLike(std::string_view arg) {
+        return arg.size() > 1 && arg.front() == '-';
+    }
 
-    for (int i = 1; i < argc; ++i) {
-        const std::string_view arg(argv[i]);
-
-        if (!optionsEnded && arg == "--") {
-            parsed.separatorIndex = i;
-            optionsEnded = true;
-            continue;
+    std::string commandTarget(std::string_view command) {
+        for (const CommandAlias& alias : kCommandAliases) {
+            if (alias.name == command) {
+                return std::string(alias.target);
+            }
         }
 
-        if (!optionsEnded && isGlobalOption(arg)) {
-            continue;
-        }
+        return {};
+    }
 
-        if (!optionsEnded && isOptionLike(arg)) {
-            parsed.error = "Unknown option: " + std::string(arg);
+    ParsedArgs parseArgs(int argc, char* argv[]) {
+        ParsedArgs parsed;
+        bool optionsEnded = false;
+
+        for (int i = 1; i < argc; ++i) {
+            const std::string_view arg(argv[i]);
+
+            if (!optionsEnded && arg == "--") {
+                parsed.separatorIndex = i;
+                optionsEnded = true;
+                continue;
+            }
+
+            if (!optionsEnded && isGlobalOption(arg)) {
+                continue;
+            }
+
+            if (!optionsEnded && isOptionLike(arg)) {
+                parsed.error = "Unknown option: " + std::string(arg);
+                return parsed;
+            }
+
+            parsed.commandIndex = i;
             return parsed;
         }
 
-        parsed.commandIndex = i;
         return parsed;
     }
 
-    return parsed;
-}
-
-std::string parentDirectory(const std::string& path) {
-    const std::size_t slash = path.find_last_of('/');
-    if (slash == std::string::npos) {
-        return {};
-    }
-    if (slash == 0) {
-        return "/";
-    }
-    return path.substr(0, slash);
-}
-
-std::string joinPath(const std::string& directory, std::string_view filename) {
-    if (directory.empty()) {
-        return std::string(filename);
-    }
-    if (directory.back() == '/') {
-        return directory + std::string(filename);
-    }
-    return directory + "/" + std::string(filename);
-}
-
-bool executableAt(const std::string& path) {
-    return !path.empty() && access(path.c_str(), X_OK) == 0;
-}
-
-std::string executableDirectoryFromProc() {
-    std::array<char, 4096> path {};
-    const ssize_t length = readlink("/proc/self/exe", path.data(), path.size() - 1);
-    if (length <= 0) {
-        return {};
+    std::string parentDirectory(const std::string& path) {
+        const std::size_t slash = path.find_last_of('/');
+        if (slash == std::string::npos) {
+            return {};
+        }
+        if (slash == 0) {
+            return "/";
+        }
+        return path.substr(0, slash);
     }
 
-    return parentDirectory(std::string(path.data(), static_cast<std::size_t>(length)));
-}
-
-std::string executableDirectoryFromArgv(const char* argv0) {
-    if (argv0 == nullptr) {
-        return {};
-    }
-
-    const std::string path(argv0);
-    return path.find('/') == std::string::npos ? std::string {} : parentDirectory(path);
-}
-
-std::string resolveComponentExecutable(std::string_view target, const char* argv0) {
-    const std::array<std::string, 3> candidateDirs {
-        executableDirectoryFromProc(),
-        executableDirectoryFromArgv(argv0),
-        "/opt/ZPM/bin",
-    };
-
-    for (const std::string& directory : candidateDirs) {
+    std::string joinPath(const std::string& directory, std::string_view filename) {
         if (directory.empty()) {
-            continue;
+            return std::string(filename);
+        }
+        if (directory.back() == '/') {
+            return directory + std::string(filename);
+        }
+        return directory + "/" + std::string(filename);
+    }
+
+    bool executableAt(const std::string& path) {
+        return !path.empty() && access(path.c_str(), X_OK) == 0;
+    }
+
+    std::string executableDirectoryFromProc() {
+        std::array<char, 4096> path {};
+        const ssize_t length = readlink("/proc/self/exe", path.data(), path.size() - 1);
+        if (length <= 0) {
+            return {};
         }
 
-        const std::string candidate = joinPath(directory, target);
-        if (executableAt(candidate)) {
-            return candidate;
+        return parentDirectory(std::string(path.data(), static_cast<std::size_t>(length)));
+    }
+
+    std::string executableDirectoryFromArgv(const char* argv0) {
+        if (argv0 == nullptr) {
+            return {};
         }
+
+        const std::string path(argv0);
+        return path.find('/') == std::string::npos ? std::string {} : parentDirectory(path);
     }
 
-    return std::string(target);
-}
+    std::string resolveComponentExecutable(std::string_view target, const char* argv0) {
+        const std::array<std::string, 3> candidateDirs {
+            executableDirectoryFromProc(),
+            executableDirectoryFromArgv(argv0),
+            "/opt/ZPM/bin",
+        };
 
-std::vector<std::string> buildComponentArgs(int argc,
-                                            char* argv[],
-                                            int commandIndex,
-                                            int separatorIndex,
-                                            std::string_view target) {
-    std::vector<std::string> args;
-    args.reserve(static_cast<std::size_t>(argc));
-    args.emplace_back(target);
+        for (const std::string& directory : candidateDirs) {
+            if (directory.empty()) {
+                continue;
+            }
 
-    for (int i = 1; i < argc; ++i) {
-        if (i == commandIndex || i == separatorIndex) {
-            continue;
+            const std::string candidate = joinPath(directory, target);
+            if (executableAt(candidate)) {
+                return candidate;
+            }
         }
-        args.emplace_back(argv[i]);
+
+        return std::string(target);
     }
 
-    return args;
-}
+    std::vector<std::string> buildComponentArgs(int argc,
+                                                char* argv[],
+                                                int commandIndex,
+                                                int separatorIndex,
+                                                std::string_view target) {
+        std::vector<std::string> args;
+        args.reserve(static_cast<std::size_t>(argc));
+        args.emplace_back(target);
 
-int execComponent(const std::string& executable, const std::vector<std::string>& args) {
-    if (executable.empty() || args.empty()) {
-        std::cerr << RED << "Error: Missing component executable.\n" << RESET;
-        return 127;
-    }
-
-    std::vector<char*> argv;
-    argv.reserve(args.size() + 1);
-    for (const std::string& arg : args) {
-        argv.push_back(const_cast<char*>(arg.c_str()));
-    }
-    argv.push_back(nullptr);
-
-    if (executable.find('/') == std::string::npos) {
-        execvp(executable.c_str(), argv.data());
-    } else {
-        execv(executable.c_str(), argv.data());
-    }
-
-    const int errorCode = errno;
-    std::cerr << RED << "Error: Could not start component '" << args.front() << "'";
-    if (executable != args.front()) {
-        std::cerr << " (" << executable << ")";
-    }
-    std::cerr << ": " << std::strerror(errorCode) << "\n" << RESET;
-
-    return errorCode == ENOENT ? 127 : 126;
-}
-
-bool hasFlag(int argc, char* argv[], std::string_view shortName, std::string_view longName) {
-    for (int i = 1; i < argc; ++i) {
-        const std::string_view arg(argv[i]);
-        if (arg == shortName || arg == longName) {
-            return true;
+        for (int i = 1; i < argc; ++i) {
+            if (i == commandIndex || i == separatorIndex) {
+                continue;
+            }
+            args.emplace_back(argv[i]);
         }
-    }
-    return false;
-}
+
+        return args;
+                                                }
+
+                                                int execComponent(const std::string& executable, const std::vector<std::string>& args) {
+                                                    if (executable.empty() || args.empty()) {
+                                                        std::cerr << RED << "Error: Missing component executable.\n" << RESET;
+                                                        return 127;
+                                                    }
+
+                                                    std::vector<char*> argv;
+                                                    argv.reserve(args.size() + 1);
+                                                    for (const std::string& arg : args) {
+                                                        argv.push_back(const_cast<char*>(arg.c_str()));
+                                                    }
+                                                    argv.push_back(nullptr);
+
+                                                    if (executable.find('/') == std::string::npos) {
+                                                        execvp(executable.c_str(), argv.data());
+                                                    } else {
+                                                        execv(executable.c_str(), argv.data());
+                                                    }
+
+                                                    const int errorCode = errno;
+                                                    std::cerr << RED << "Error: Could not start component '" << args.front() << "'";
+                                                    if (executable != args.front()) {
+                                                        std::cerr << " (" << executable << ")";
+                                                    }
+                                                    std::cerr << ": " << std::strerror(errorCode) << "\n" << RESET;
+
+                                                    return errorCode == ENOENT ? 127 : 126;
+                                                }
+
+                                                bool hasFlag(int argc, char* argv[], std::string_view shortName, std::string_view longName) {
+                                                    for (int i = 1; i < argc; ++i) {
+                                                        const std::string_view arg(argv[i]);
+                                                        if (arg == shortName || arg == longName) {
+                                                            return true;
+                                                        }
+                                                    }
+                                                    return false;
+                                                }
 
 } // namespace
 
@@ -269,7 +271,7 @@ int main(int argc, char* argv[]) {
 
         const std::string executable = resolveComponentExecutable(target, argv[0]);
         const std::vector<std::string> componentArgs =
-            buildComponentArgs(argc, argv, parsed.commandIndex, parsed.separatorIndex, target);
+        buildComponentArgs(argc, argv, parsed.commandIndex, parsed.separatorIndex, target);
         return execComponent(executable, componentArgs);
     }
 
