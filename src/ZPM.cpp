98e20c4441b3670638fda980507135c39332b844
Ignac <ignacyzielinski2008@gmail.com>
2026-06-14 14:07:03 +0200

    Add TUI command to ZPM command list

--- src/ZPM.cpp
+++ src/ZPM.cpp
@@ -39,6 +39,7 @@ constexpr std::array<CommandAlias, 16> kCommandAliases {{
     {"uninstall", "zuninstall"},
     {"home", "zhome"},
     {"run", "zrun"},
+    {"tui", "ztui"},
 }};
 
 constexpr std::array<CommandHelp, 11> kCommandHelp {{
@@ -53,7 +54,7 @@ constexpr std::array<CommandHelp, 11> kCommandHelp {{
     {"info", "Package information"},
     {"uninstall", "Uninstall ZPM"},
     {"run", "Run programs using ZPM"},
-    {"tui", "Run ZPM terminal UI"}
+    {"tui", "Run ZPM terminal UI"},
 }};
 
 void showVersion() {
