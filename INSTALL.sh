4e5e1e736a23219dd028692250e97b3292dbde4c
Ignac <ignacyzielinski2008@gmail.com>
2026-06-15 22:26:35 +0200

    Add 'tar' to required dependencies in INSTALL.sh
    Added 'tar' to the list of required dependencies for APT, ZYPPER, and DNF package managers.

--- INSTALL.sh
+++ INSTALL.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 # ── CONSTANTS ─────────────────────────────────────────────────────────────────
 readonly LOG="/tmp/ZPM_INSTALL.log"
 readonly TARGET="/opt/ZPM"
-readonly REQUIRED_DEPS_APT=(curl git wget python3 g++ sudo zip sed gawk coreutils nano unzip libftxui-dev)
-readonly REQUIRED_DEPS_ZYPPER=(curl git wget python3 gcc-c++ sudo zip sed gawk coreutils nano unzip ftxui-devel)
-readonly REQUIRED_DEPS_DNF=(curl git wget python3 gcc-c++ sudo zip sed gawk coreutils nano unzip ftxui-devel)
+readonly REQUIRED_DEPS_APT=(curl git wget python3 g++ sudo zip sed gawk coreutils nano unzip libftxui-dev tar)
+readonly REQUIRED_DEPS_ZYPPER=(curl git wget python3 gcc-c++ sudo zip sed gawk coreutils nano unzip ftxui-devel tar)
+readonly REQUIRED_DEPS_DNF=(curl git wget python3 gcc-c++ sudo zip sed gawk coreutils nano unzip ftxui-devel tar)
 
 # ── LOGGING ───────────────────────────────────────────────────────────────────
 exec > >(tee -a "$LOG") 2>&1
