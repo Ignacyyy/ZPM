1a5d35f47b146ee397d128c0f4ffc53a67593e43
Ignac <ignacyzielinski2008@gmail.com>
2026-06-18 21:19:04 +0200

    Add GRAY color constant to colors.h

--- src/common/colors.h
+++ src/common/colors.h
@@ -8,6 +8,7 @@
 inline const std::string RED    = "\033[1;31m";
 inline const std::string GREEN  = "\033[1;32m";
 inline const std::string YELLOW = "\033[33m";
+inline const std::string GRAY   = "\033[90m";
 inline const std::string CYAN   = "\033[36m";
 inline const std::string BOLD   = "\033[1m";
 inline const std::string RESET  = "\033[0m";
